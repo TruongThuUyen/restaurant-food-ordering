f171e00c89ac6381d6d0e0221253940f0f0689cf
May0105 <88717349+May0105@users.noreply.github.com>
2025-11-09 16:19:31 +0700

    feat: implement cart, login, menu, category, orders, products, product-id, cartIcon, countDown, featured, ooffer and price component

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 
 const CartIcon = () => {
   return (
-    <Link href='/card' className='flex gap-4 items-center'>
+    <Link href='/cart' className='flex gap-4 items-center'>
       <div className='relative w-8 h-8 md:w-5 md:h-5'>
         <Image src='/cart.png' alt='card' fill />
       </div>
--- components/Featured.tsx
+++ components/Featured.tsx
@@ -11,23 +11,18 @@ const Featured = () => {
         {featuredProducts?.map((item) => (
           <div
             key={item.id}
-            className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-100 transitoin-all duration-300'>
+            className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-100 transitoin-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h[90vh]'>
             {/* IMAGE CONTAINER */}
             {item.img && (
-              <div className='relative flex-1 w-full'>
-                <Image
-                  src={item.img}
-                  alt=''
-                  fill
-                  className='object-contain hover:rotate-[60deg] transition-all duration-300'
-                />
+              <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+                <Image src={item.img} alt='' fill className='object-contain' />
               </div>
             )}
             {/* TEXT CONTAINER */}
-            <div className='flex-1 flex flex-col items-center gap-4 text-center'>
+            <div className='flex-1 flex flex-col justify-center items-center gap-4 text-center'>
               <h1 className='text-xl font-bold uppercase'>{item.title}</h1>
-              <p className='px-4'>{item.desc}</p>
-              <span className='text-xl font-bold'>{item.price}</span>
+              <p className='px-4 2xl:px-8'>{item.desc}</p>
+              <span className='text-xl font-bold xl:text-2xl 2xl:text-3xl'>{item.price}</span>
               <button className='bg-red-500 text-white p-2 rounded-md'>Add to Card</button>
             </div>
           </div>
