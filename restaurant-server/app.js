3ca9f37eb7c27a76f3eb37772120429b7e4a9d26
May0105 <88717349+May0105@users.noreply.github.com>
2025-11-13 15:48:00 +0700

    Create Product.js file and create api in this file and import productRouter in app.js

--- restaurant-server/app.js
+++ restaurant-server/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectDB = require('./config/db');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
+var productRouter = require('./routes/products');
 var usersRouter = require('./routes/users');
 
 // Connect to databse
@@ -32,8 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/products', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
