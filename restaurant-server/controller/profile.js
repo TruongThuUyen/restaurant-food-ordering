b078b32ccbc99736e1d29ef0826eb419f4f9bbfa
May0105 <88717349+May0105@users.noreply.github.com>
2025-11-22 22:13:33 +0700

    Update controller profile: change post -> get

--- restaurant-server/controller/profile.js
+++ restaurant-server/controller/profile.js
@@ -3,9 +3,9 @@ const User = require('../models/User');
 const profile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select('-password');
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ status: 2000, success: true, data: user });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Server error' });
+    res.status(500).json({ status: 4000, success: false, message: 'Server error' });
   }
 };
 
